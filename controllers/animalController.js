8b62e4993ec38d3000caa87f4b58aee431b0dc5e
macas5 <mkalinauskas1993@gmail.com>
2022-09-13 17:29:36 +0300

    make controllers more reusable

--- controllers/animalController.js
+++ controllers/animalController.js
@@ -2,23 +2,19 @@ import birdModel from '../models/birdModel.js'
 import dogModel from '../models/dogModel.js'
 import catModel from '../models/catModel.js'
 
+const animalModels = {
+    ["bird"]: birdModel,
+    ["dog"]: dogModel,
+    ["cat"]: catModel            
+}
+
 export const createAnimal = async (req, res) => {
     try {
-        const type = req.params.animalType;
-        const newAnimal = type == "bird" ? 
-            new birdModel({
+        const newAnimal = new animalModels[req.params.animalType]({
                 ...req.body
             })
-            : type == "dog" ?
-            new dogModel({
-                ...req.body
-            })
-            :
-            new catModel({
-                ...req.body
-            });
         await newAnimal.save();
-        return res.status(201).send(`New ${type} entry created`);
+        return res.status(201).send(`New ${req.params.animalType} entry created`);
     } catch (error) {
         console.error(error);
         res.status(405).send(error);
@@ -40,13 +36,7 @@ export const getAnimals = async (req, res) => {
 
 export const getAnimalByCondition = async (req, res) => {
     try {
-        const type = req.params.animalType;
-        const animal = type == "bird" ? 
-            await birdModel.find(req.body)
-            : type == "dog" ?
-            await dogModel.find(req.body)
-            :
-            await catModel.find(req.body); 
+        const animal = await animalModels[req.params.animalType].find(req.body)
         res.status(202).json(animal);
     } catch (error) {
         console.error(error);
@@ -56,13 +46,8 @@ export const getAnimalByCondition = async (req, res) => {
 
 export const updateAnimalByCondition = async (req, res) => {
     try {
-        const type = req.params.animalType;
-        const animal = type == "bird" ? 
-            await birdModel.updateOne(req.body[0], {$set: req.body[1]}, {new: true})
-            : type == "dog" ?
-            await dogModel.updateOne(req.body[0], {$set: req.body[1]}, {new: true})
-            :
-            await catModel.updateOne(req.body[0], {$set: req.body[1]}, {new: true}); 
+        const animal = await animalModels[req.params.animalType]
+        .updateOne(req.body[0], {$set: req.body[1]});
         res.status(202).json(animal);
     } catch (error) {
         console.error(error);
@@ -72,13 +57,7 @@ export const updateAnimalByCondition = async (req, res) => {
 
 export const deleteAnimalByCondition = async (req, res) => {
     try {
-        const type = req.params.animalType;
-        type == "bird" ? 
-            await birdModel.deleteOne(req.body)
-            : type == "dog" ?
-            await dogModel.deleteOne(req.body)
-            :
-            await catModel.deleteOne(req.body); 
+       await animalModels[req.params.animalType].deleteOne(req.body)
         res.status(202).send('Animal was successfuly deleted');
     } catch (error) {
         console.error(error);
